c45f88c9509b1a2f3d3b1f49237a3487fa584f11
Nikolay Radonich <radonich@smarttender.biz>
2023-04-05 11:38:23 +0200

    fixed config

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,16 +8,15 @@ return {
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    config = function()
-      require("neo-tree").setup {
-        filesystem = {
-          filtered_items = {
-            visible = true,
-            hide_dotfiles = false,
-            hide_gitignored = true,
-          },
-        }
+    config = true,
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
       }
-    end
+    },
   }
 }
